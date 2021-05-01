088c93d94cea4aa55499e0b4540c7d4ff34d9458
Leonardo Silva <silvaleonardo.ti@gmail.com>
2021-05-01 11:01:16 -0300

    Add: added Dashboard page

---
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { DarkModeCheckbox } from "../../shared/components/dark-mode-checkbox/dark-mode-checkbox";
+import "./Dashboard.css";
+
+export const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard-base flex-content-center flex-items-center">
+      Olá dashboard
+      {/* <DarkModeCheckbox /> */}
+    </div>
+  );
+};
--- src/app/routes/PrivateRoutes.tsx
+++ src/app/routes/PrivateRoutes.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import { Dashboard } from "../pages";
 
 export const PrivateRoutes: React.FC = () => {
   return (
     <Switch>
-      {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+      <Route exact path="/dashboard" component={Dashboard} />
 
       <Route exact path="*" component={() => <Redirect to="/dashboard" />} />
     </Switch>

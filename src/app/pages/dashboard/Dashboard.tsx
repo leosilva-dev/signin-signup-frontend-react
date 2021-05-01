5c18262f0a2df72ecd0604ce75667027b0ba366b
Leonardo Silva <silvaleonardo.ti@gmail.com>
2021-05-01 12:11:49 -0300

    feat: added button to logout

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { DarkModeCheckbox } from "../../shared/components";
-
+import { Button, DarkModeCheckbox } from "../../shared/components";
+import { useAuth } from "../../shared/hooks";
 import "./Dashboard.css";
 
 export const Dashboard: React.FC = () => {
+  const { logout } = useAuth();
   return (
-    <div className="dashboard-base flex-content-center flex-items-center">
+    <div className="dashboard-base flex-content-center flex-items-center flex-column">
       Olá dashboard
+      <Button onClick={logout}>Logout</Button>
       <DarkModeCheckbox />
     </div>
   );
